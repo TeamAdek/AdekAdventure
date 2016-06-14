8cc87947c21aa4dbd939f22cbcdef2e44bc0e394
Alexander <alexander_29@abv.bg>
2016-06-15 01:18:12 +0300

    Sort collison tiles.

--- DaGeim/DaGeim/src/Level/Map.cs
+++ DaGeim/DaGeim/src/Level/Map.cs
@@ -1,15 +1,14 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace DaGeim
 {
     class Map
     {
+        private int tileSize;
         private List<CollisionTiles> collisionTiles = new List<CollisionTiles>();
 
         public List<CollisionTiles> CollisionTiles
@@ -18,9 +17,15 @@ namespace DaGeim
         }
         private int widht, height;
 
+        public int TileSize
+        {
+            get { return this.tileSize; }
+            set { this.tileSize = value; }
+        }
+
         public int Widht
         {
-            get { return widht;  }
+            get { return widht; }
         }
         public int Height
         {
@@ -29,23 +34,43 @@ namespace DaGeim
 
         public Map()
         {
+            this.TileSize = 72; // size of single tiile
+        }
+        public void Load(Map map)
+        {
+            map.Generate(new int[,]{
+
+                      { 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5},
+                      { 6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+                      { 6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,5,5,5,5,5,5,5,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+                      { 6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,5,5,5,5,5,5,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+                      { 6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,0,0,0,0,0,5,5,5,5,5,5,5,5,5,0,0,0,0,0,1,2,2,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+                      { 6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,5,5,3,0,0,0,0,0,5,5,5,5,5,5,5,0,0,0,0,0,1,5,5,5,5,5,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+                      { 6,0,0,0,0,0,0,0,0,1,2,3,0,0,1,2,3,0,0,0,0,0,1,2,2,2,2,2,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,5,5,5,5,5,5,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+                      { 6,0,0,0,0,1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,5,5,5,5,5,5,5,5,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,5,5,5,5,5,5,5,5,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+                      { 6,0,0,0,1,5,5,3,0,0,0,0,0,0,0,0,0,0,1,2,5,5,5,5,5,5,5,5,5,5,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,5,5,5,5,5,5,5,5,5,5,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,5},
+                      { 5,2,2,2,5,5,5,5,2,2,2,2,2,2,2,2,2,2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5}, //10
+
+               }, this.tileSize);
 
         }
 
-        public void Generate(int[,] map , int size)
+        public void Generate(int[,] map, int size)
         {
             for (int x = 0; x < map.GetLength(1); x++)
+            {
                 for (int y = 0; y < map.GetLength(0); y++)
                 {
                     int number = map[y, x];
 
-                    if (number > 0)
-                        collisionTiles.Add(new CollisionTiles(number, new Rectangle(x * size, y * size, size, size)));
+                    if (number > 0) collisionTiles.Add(new CollisionTiles(number, new Rectangle(x * size, y * size, size, size)));
 
                     widht = (x + 1) * size;
                     height = (y + 1) * size;
                 }
-            
+            }
+
+            this.collisionTiles.Sort();
         }
         public void Draw(SpriteBatch spriteBatch)
         {
