4790ee90065011d7beb593986f512b13e8428f94
Alexander <alexander_29@abv.bg>
2016-06-15 02:56:24 +0300

    add collision with map

--- DaGeim/DaGeim/src/Helper Classes/Rockets.cs
+++ DaGeim/DaGeim/src/Helper Classes/Rockets.cs
@@ -7,33 +7,41 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-public class Rockets 
+public class Rockets
+{
+    public Texture2D shootTexture;
+    public Vector2 shootPosition;
+    public bool isVisible;
+    public float speed;
+    public string direction;
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Rocket Constructor
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public Rockets(Texture2D newBullet)
     {
-        public Texture2D shootTexture;
-        public Vector2 shootPosition;
-        public bool isVisible;
-        public float speed;
-        public string direction;
-
-        //-----------------------------------------------------------------------------------------------------------------------------------------------------------
-        // Rocket Constructor
-        //-----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-        public Rockets(Texture2D newBullet)
-        {
-            speed = 5;
-            shootTexture = newBullet;
-            isVisible = false;
-            direction = "right";
-        }
-
-        //-----------------------------------------------------------------------------------------------------------------------------------------------------------
-        // Draw rockets
-        //-----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(shootTexture, shootPosition, Color.White);
-        }
-
+        speed = 5;
+        shootTexture = newBullet;
+        isVisible = false;
+        direction = "right";
     }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Draw rockets
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(shootTexture, shootPosition, Color.White);
+    }
+
+    public void Collision(Rectangle otherObject)
+    {
+        Rectangle thisRectangle = new Rectangle((int)this.shootPosition.X, (int)this.shootPosition.Y, this.shootTexture.Width, this.shootTexture.Height);
+        if (thisRectangle.Intersects(otherObject))
+        {
+            this.isVisible = false;
+        }
+    }
+}
