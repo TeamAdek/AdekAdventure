5a209d495a8f900d97cda95094f17014a8aae2e2
Alexander <alexander_29@abv.bg>
2016-06-15 00:26:48 +0300

    Interface for entities

---
+++ DaGeim/DaGeim/src/Entities/IEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace Game.src.Entities
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    
+    public interface IEntity
+    {
+        /// <summary>
+        /// Draw rectangle of entity
+        /// </summary>
+        Rectangle CollisionBox { get; set; }
+
+        /// <summary>
+        /// Draw metod of entity
+        /// </summary>
+        /// <param name="spriteBach"></param>
+        void Draw(SpriteBatch spriteBach);
+
+        /// <summary>
+        /// Make collision with map
+        /// </summary>
+        /// <param name="tileRectangle"></param>
+        void CollisionWithMap(Rectangle tileRectangle);
+    }
+}
